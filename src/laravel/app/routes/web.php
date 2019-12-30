1ccb3ceb3e04e517bd3bc4ea2bc8024ae3a31a50
kawa1228 <sxa.1105bo@gmail.com>
2019-12-30 19:34:01 +0900

    ルートを定義

--- src/laravel/app/routes/web.php
+++ src/laravel/app/routes/web.php
@@ -11,6 +11,17 @@
 |
 */
 
+use App\Task;
+use Illuminate\Http\Request;
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('tasks');
+});
+
+Route::post('/task', function (Request $request) {
+    //
+});
+
+Route::delete('/task/{task}', function (Task $task) {
+    //
 });
